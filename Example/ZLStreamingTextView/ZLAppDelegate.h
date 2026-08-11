3237bef2554df3e88385491d8a93df09b954288c
fanpeng <peng.fan@ukelink.com>
2026-08-11 16:57:18 +0800

    Initial commit

---
+++ Example/ZLStreamingTextView/ZLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZLAppDelegate.h
+//  ZLStreamingTextView
+//
+//  Created by fanpeng on 08/11/2026.
+//  Copyright (c) 2026 fanpeng. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ZLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
